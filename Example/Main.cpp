aaca429ce9c686191cdb48b32e96d0e138572637
Johannes <52950755+Sumandora@users.noreply.github.com>
2023-08-21 16:18:56 +0200

    Update cmake usage and add tests

--- Example/Main.cpp
+++ Example/Main.cpp
@@ -14,11 +14,15 @@ int main()
 	std::optional<std::uintptr_t> uintptr = *null;
 	assert(!uintptr.has_value());
 
+	printf("Null can't be dereferenced\n");
+
 	float f = 1.0f;
 	SafePointer<float> floatPtr = &f;
 
 	std::optional<float> floatDeref = *floatPtr;
 	assert(floatDeref.has_value());
+	printf("However this float is valid, so we can dereference it\n");
 
 	assert(floatDeref.value() == f);
+	printf("As you can see: %f = %f\n", floatDeref.value(), f);
 }
